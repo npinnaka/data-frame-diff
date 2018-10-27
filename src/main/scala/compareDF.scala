8eaf9385f6f724c964bba820dcc1e2ed4b562d78
Narendra Pinnaka <learnnew@outlook.com>
2018-10-27 06:49:33 -0500

    updated code to show diff in columns and diff in data

--- src/main/scala/compareDF.scala
+++ src/main/scala/compareDF.scala
@@ -22,32 +22,36 @@ def lookDiff(data_frame1: DataFrame, data_frame2: DataFrame): Long = {
     println(data_frame1.inputFiles.mkString(",\n"))
     println("against ")
     println(data_frame2.inputFiles.mkString(",\n"))
-    val df1_df2_df = data_frame1.except(data_frame2)
-    total_diff = df1_df2_df.count
-    if (total_diff > 0) {
-      println("differences on files...")
-      df1_df2_df.show
-      return total_diff
-    }
-    else {
-      println("data matches.....")
-    }
-  }
-  else {
     val columns = data_frame1.schema.fields.map(_.name)
-    val selectiveDifferences = columns.map(col => data_frame1.select(col).except(data_frame2.select(col)))
-    selectiveDifferences.map(diff => {
+    val df1_df2_df = columns.map(col => {
+      data_frame1.select(col).except(data_frame2.select(col))
+    })
+    df1_df2_df.map(diff => {
       total_diff = diff.count
       if (total_diff > 0) {
         diff.show
-        return total_diff
+      }
+      else {
+        println("data matches.....")
       }
     })
   }
+  else {
+    println("checking for difference in files...")
+    println(data_frame1.inputFiles.mkString(",\n"))
+    println("against ")
+    println(data_frame2.inputFiles.mkString(",\n"))
+    val diff_1_2 = data_frame1.columns.toSet.diff(data_frame2.columns.toSet)
+    total_diff = diff_1_2.size
+    if (total_diff > 0) {
+      println("missing fields...[" + diff_1_2.mkString(", ") + "]")
+    }
+  }
+  println("*****************************iteration compiled***************************************")
   return total_diff
 }
 
-def comapreDF(sourceFile :String , destinationFile : String) : Unit = {
+def comapreDF(sourceFile: String, destinationFile: String): Unit = {
   val df1: DataFrame = spark.read.json(sourceFile)
   val df2: DataFrame = spark.read.json(destinationFile)
   df1.registerTempTable("employee1")
@@ -66,7 +70,7 @@ def comapreDF(sourceFile :String , destinationFile : String) : Unit = {
   }
 }
 
-comapreDF("employee1.json", "employee2.json")
+comapreDF("resources/employee1.json",  "resources/employee2.json")
 System.exit(0)
 
 
