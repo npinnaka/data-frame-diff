2c3bf97513a9c99fe3ab3627e6409c8a7598e3dc
Narendra Pinnaka <npinnaka@yahoo.com>
2018-11-01 19:09:42 -0500

    updated compare function...

--- src/main/scala/compareDF.scala
+++ src/main/scala/compareDF.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.sql.DataFrame
+import scala.collection.immutable.Map
 
-def preapreSQL(data_frame: DataFrame, table_name: String): String = {
+def prepareSql(data_frame: DataFrame, table_name: String): String = {
+  data_frame.registerTempTable(table_name)
   return "select "
     .concat(data_frame.columns.filter(_ != "address")
       .toSeq
@@ -11,44 +13,54 @@ def preapreSQL(data_frame: DataFrame, table_name: String): String = {
       .sorted
       .mkString(", "))
     .concat(" from ")
-    .concat(table_name);
+    .concat(table_name)
+}
+
+def createMap(data_frame: DataFrame): Map[String, Any] = {
+  data_frame.show
+  return data_frame.columns.toList zip data_frame.collect.head.toSeq toMap
 }
 
 def lookDiff(data_frame1: DataFrame, data_frame2: DataFrame): Long = {
-  var total_diff: Long = 0
   if (data_frame1.columns.sameElements(data_frame2.columns)) {
     println("columns are matching now comparing data...")
     println("checking for difference in files...")
-    println(data_frame1.inputFiles.mkString(",\n"))
+    println(data_frame1.inputFiles.mkString(","))
     println("against ")
-    println(data_frame2.inputFiles.mkString(",\n"))
-    val columns = data_frame1.schema.fields.map(_.name)
-    val df1_df2_df = columns.map(col => {
-      data_frame1.select(col).except(data_frame2.select(col))
-    })
-    df1_df2_df.map(diff => {
-      total_diff = diff.count
-      if (total_diff > 0) {
-        diff.show
+    println(data_frame2.inputFiles.mkString(","))
+    data_frame1.select("id").collect.foreach(row => {
+      val id: Long = row.getLong(0)
+      println("comparing data for Id:" + id)
+      val map1 = createMap(data_frame1.filter("id = " + id))
+      val map2 = createMap(data_frame2.filter("id = " + id))
+      println("Creted map1 " + map1)
+      println("Creted map2 " + map2)
+      val diff1_2 = (map1.toSet diff map2.toSet).toMap
+      val diff2_1 = (map2.toSet diff map1.toSet).toMap
+      if (diff1_2 != Set.empty || diff2_1 != Set.empty) {
+        println("differences from 1 -> 2 " + diff1_2)
+        println("differences from 2 -> 1 " + diff2_1)
+        return -1
       }
       else {
-        println("data matches.....")
+        println("records are good")
       }
+      println("***********completed comparing data for Id:" + id)
     })
   }
   else {
     println("checking for difference in files...")
-    println(data_frame1.inputFiles.mkString(",\n"))
+    println(data_frame1.inputFiles.mkString(","))
     println("against ")
-    println(data_frame2.inputFiles.mkString(",\n"))
+    println(data_frame2.inputFiles.mkString(","))
     val diff_1_2 = data_frame1.columns.toSet.diff(data_frame2.columns.toSet)
-    total_diff = diff_1_2.size
-    if (total_diff > 0) {
+    if (diff_1_2.size > 0) {
       println("missing fields...[" + diff_1_2.mkString(", ") + "]")
+      return -1
     }
   }
   println("*****************************iteration compiled***************************************")
-  return total_diff
+  return 0;
 }
 
 def comapreDF(sourceFile: String, destinationFile: String): Unit = {
@@ -57,8 +69,8 @@ def comapreDF(sourceFile: String, destinationFile: String): Unit = {
   df1.registerTempTable("employee1")
   df2.registerTempTable("employee2")
 
-  val sqlQueryEmployee1 = preapreSQL(df1, "employee1")
-  val sqlQueryEmployee2 = preapreSQL(df2, "employee2")
+  val sqlQueryEmployee1 = prepareSql(df1, "employee1")
+  val sqlQueryEmployee2 = prepareSql(df2, "employee2")
   val emp_df1 = spark.sql(sqlQueryEmployee1)
   val emp_df2 = spark.sql(sqlQueryEmployee2)
 
@@ -70,7 +82,7 @@ def comapreDF(sourceFile: String, destinationFile: String): Unit = {
   }
 }
 
-comapreDF("resources/employee1.json",  "resources/employee2.json")
+comapreDF("resources/employee1.json", "resources/employee3.json")
 System.exit(0)
 
 
